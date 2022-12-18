629a42095922e94bf7cb54386e7a427345f85f71
gjon94 <gjongjoka94@hotmail.it>
2022-12-18 02:11:43 +0100

    [1.4.9]

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const urls = [
   "https://cdn.jsdelivr.net/gh/gjon94/weatherCdn@f438ecc3049dd6bbfff2405c6003dff528d665e8/finalClass.js",
-  "https://cdn.jsdelivr.net/gh/gjon94/weatherCdn@f438ecc3049dd6bbfff2405c6003dff528d665e8/searchCities.js",
-  "https://cdn.jsdelivr.net/gh/gjon94/weatherCdn@f438ecc3049dd6bbfff2405c6003dff528d665e8/weatherDispatcher.js",
+  "https://cdn.jsdelivr.net/gh/gjon94/weatherCdn@e7847f403df7d04d55c9db10aa94a5deec38b2a5/searchCities.js",
+  "https://cdn.jsdelivr.net/gh/gjon94/weatherCdn@e7847f403df7d04d55c9db10aa94a5deec38b2a5/weatherDispatcher.js",
   "https://cdn.jsdelivr.net/gh/gjon94/weatherCdn@f438ecc3049dd6bbfff2405c6003dff528d665e8/dispatcher.js",
 ];
 window.weatherCdn = {};
