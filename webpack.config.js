e85ea6e013723c1e06f813092097008e4e5ab422
gjon94 <gjongjoka94@hotmail.it>
2022-12-22 04:05:02 +0100

    [1.7.3]

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
-  mode: "development",
+  mode: "production",
   entry: "./src/index.js",
   plugins: [new CleanWebpackPlugin(), new HtmlWebpackPlugin({})],
   output: {
